786c8f27a95cc3d4080b4ed63febf12c3859246e
Hussein Sayed Ahmad <262915033+husseinsayedah@users.noreply.github.com>
2026-09-14 16:05:58 +0200

    feat(tp01): add Model and Task types

---
+++ tp01/src/model.ts
@@ -0,0 +1,15 @@
+export type Task =
+  | "text-generation"
+  | "translation"
+  | "image-classification"
+  | "speech-to-text";
+
+export interface Model {
+  id: string;
+  name: string;
+  org: string;
+  task: Task;
+  parameters: number;
+  downloads: number;
+  license?: string;
+}
